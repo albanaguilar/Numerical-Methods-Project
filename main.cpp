873b8277838ff7d3004db1b3b97ff86e40411aaa
Albán <albanaguilar1@gmail.com>
2017-06-25 16:32:48 -0500

    Primer avance

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int Menu( ){
+    int iOpcion;
+    cout << "Seleccione el numero del metodo que desee: " << endl;
+    cout << "   1.- Trapezoidal" << endl;
+    cout << "   2.- Simpson 1/3" << endl;
+    cout << "   3.- Simpson 3/8" << endl;
+    cout << "   4.- Secante" << endl;
+    cout << "   5.- Newton-Raphson" << endl;
+    cout << "   6.- Biseccion" << endl;
+    cout << "   7.- Montante" << endl;
+    cout << "   8.- Gauss" << endl;
+    cout << "   9.- Interpolacion" << endl;
+    cin >> iOpcion;
+    return iOpcion;
+}
+
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
